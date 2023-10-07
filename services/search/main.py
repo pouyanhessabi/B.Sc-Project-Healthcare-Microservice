b6e9da982991fce0f0fc3670081e52dd868b39db
pouyan.hessabi <pouyan.hessabi@gmail.com>
2023-10-07 23:58:01 +0330

    search service added.

--- services/search/main.py
+++ services/search/main.py
@@ -1,5 +1,7 @@
+import os
+
 import mysql.connector
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,6 +17,52 @@ mysql_db = mysql.connector.connect(
 )
 
 
-@app.route('/search_clinic')
-def search_clinic(expertise):
-    return f"expert is {expertise}"
+@app.route('/')
+def index():
+    expertises = read_expertises()
+    return render_template('default.html', expertises=expertises)
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    expertise = request.form.get('expertise')
+    doctors_tuple = load_doctors_by_expertise(expertise)
+    doctors = create_doctors_list(doctors_tuple)
+    print('expertise search', expertise)
+    print('doctors', doctors)
+    return render_template('search.html', expertise=expertise, doctors=doctors)
+
+
+def load_doctors_by_expertise(expertise: str):
+    my_cursor = mysql_db.cursor()
+    where_data = (expertise,)
+    my_cursor.execute('select * from doctor where expertises = %s order by rate DESC', where_data)
+    doctors = my_cursor.fetchall()
+    return doctors
+
+
+def doctor_tuple_to_dictionary(doctor: tuple):
+    id_, email, password, name, code, rate, expertises, clinic = doctor
+    doctor_dictionary = {
+        'id': id_, 'email': email, 'password': password, 'name': name, 'code': code, 'rate': rate,
+        'expertises': expertises, 'clinic': clinic
+    }
+
+    return doctor_dictionary
+
+
+def create_doctors_list(doctors: list):
+    doctors_list = []
+    for doctor in doctors:
+        doctors_list.append(doctor_tuple_to_dictionary(doctor))
+    return doctors_list
+
+
+def read_expertises():
+    items = []
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'expertises.txt')
+    with open(file_path, 'r') as file:
+        for line in file:
+            items.append(line.strip())
+    return items
